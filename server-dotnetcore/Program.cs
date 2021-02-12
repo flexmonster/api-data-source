873477f1c0ec4efc5bef36ec3ffffc156229bd5f
olena <olena.krysan@gmail.com>
2021-02-12 19:56:42 +0200

    Change IHostBuilder initial configuration

--- server-dotnetcore/Program.cs
+++ server-dotnetcore/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using Microsoft.Extensions.Logging.EventLog;
+using Microsoft.Extensions.Logging;
 
 namespace NetCoreServer
 {
@@ -20,6 +24,7 @@ namespace NetCoreServer
             };
         public static void Main(string[] args)
         {
+
             CreateHostBuilder(args).Build().Run();
         }
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -39,18 +44,59 @@ namespace NetCoreServer
                 .AddJsonFile(defaultConfigFilename)
                 .AddCommandLine(args, Program.ARGS_MAPPING);
             configuration = configBuilder.Build();
+            var hostBuilder = new HostBuilder();
 
-            var hostBuilder = Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, configBuilder) =>
+            hostBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+            hostBuilder.ConfigureHostConfiguration(config =>
+            {
+                config.AddEnvironmentVariables(prefix: "DOTNET_");
+                if (args != null)
                 {
-                    configBuilder.AddConfiguration(configuration);
-                })
+                    config.AddCommandLine(args);
+                }
+            });
+
+            hostBuilder.ConfigureAppConfiguration((hostingContext, configBuilder) =>
+            {
+                IHostEnvironment env = hostingContext.HostingEnvironment;
+                bool reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
+                configBuilder.AddConfiguration(configuration);
+                configBuilder.AddEnvironmentVariables();
+                if (args != null)
+                {
+                    configBuilder.AddCommandLine(args);
+                }
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
                         .UseUrls("http://0.0.0.0:" + configuration.GetValue<string>("port"));
-                });
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
+                    if (isWindows)
+                    {
+                        logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning);
+                    }
+
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+
+                    if (isWindows)
+                    {
+                        logging.AddEventLog();
+                    }
+                })
+            .UseDefaultServiceProvider((context, options) =>
+            {
+                bool isDevelopment = context.HostingEnvironment.IsDevelopment();
+                options.ValidateScopes = isDevelopment;
+                options.ValidateOnBuild = isDevelopment;
+            });
             return hostBuilder;
         }
     }
